79dca3df5f2c0f522567b3677322a62034c1b49d
akihikoisoe <180363344+akiso999@users.noreply.github.com>
2024-11-14 21:27:55 +0900

    カレンダー部分修正

--- src/main/java/com/dmm/task/MainController.java
+++ src/main/java/com/dmm/task/MainController.java
@@ -1,33 +1,48 @@
 
 package com.dmm.task;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import antlr.collections.List;
 
 @Controller
 public class MainController {
 
-	@RequestMapping("/create")
+	@GetMapping("/create")
 	public String index() {
 		return "create";
 	}
 
-	@RequestMapping("/edit")
-	public String edit() {
-		return "edit2";
+	@GetMapping("/edit")
+	public String test() {
+		return "edit";
 	}
-	
-//	@RequestMapping("/testedit")
-//	public String test() {
-//		return "edit";
-//	}
 
-	@RequestMapping("/main")
+	@GetMapping("/main")
 	public String main() {
-		return "main";
-	}
 
-	@RequestMapping("/login")
+	    // 週と日を格納する二次元配列を用意する
+	    List<List<LocalDate>> month = new ArrayList<>();
+	    
+	    // 1週間分のLocalDateを格納するListを用意する
+	    List<LocalDate> week = new ArrayList<>();
+	    
+	 // 日にちを格納する変数を用意する
+	    LocalDate day;
+
+	    // その月の1日を取得する
+	    day = LocalDate.now();  // 現在日時を取得
+	    day = LocalDate.of(day.getYear(), day.getMonthValue(), 1);  // 現在日時からその月の1日を取得
+	    
+	    
+	}
+		
+	
+	@GetMapping("/login")
 	public String login() {
 		return "login";
 	}
