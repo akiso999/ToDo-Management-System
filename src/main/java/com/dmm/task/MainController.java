c3d95d34fcbb2865bc143bdc4ab68773cb242e6c
akihikoisoe <180363344+akiso999@users.noreply.github.com>
2024-11-15 10:54:44 +0900

    修正11/15

--- src/main/java/com/dmm/task/MainController.java
+++ src/main/java/com/dmm/task/MainController.java
@@ -1,13 +1,13 @@
 
 package com.dmm.task;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import antlr.collections.List;
 
 @Controller
 public class MainController {
@@ -38,7 +38,34 @@ public class MainController {
 	    day = LocalDate.now();  // 現在日時を取得
 	    day = LocalDate.of(day.getYear(), day.getMonthValue(), 1);  // 現在日時からその月の1日を取得
 	    
+	    // 前月分の LocalDateを求める
+	    DayOfWeek w = day.getDayOfWeek();  // 当該日の曜日を取得
+	    day = day.minusDays(w.getValue());  // 1日からマイナスして 10/27を取得
 	    
+	    
+	    //繰り返す
+	    for(int i = 1; i <= 7; i++) {
+	    
+	    	//最終週かどうかの判定
+	    	if (条件式){
+	    		  
+	    		}else{
+	    		  
+	    		}
+	    	
+	    	// 1週目（1日ずつ増やして 週のリストに格納していく）
+	    	for(int j = 1; j <= 7; j++) {
+	    		week.add(day);  // 週のリストへ格納
+	    		day = day.plusDays(1);  // 1日進める
+	    	}
+	    	month.add(week);  // 1週目のリストを、月のリストへ格納する
+
+	    	week = new ArrayList<>();  // 次週のリストを新しくつくる
+	    
+	    	
+	    }
+	    return "main";
+	   
 	}
 		
 	
