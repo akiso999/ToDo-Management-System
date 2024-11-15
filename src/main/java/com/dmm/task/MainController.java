824ee08d0ea19a34094c2c0c2692d8c5e46c278b
akihikoisoe <180363344+akiso999@users.noreply.github.com>
2024-11-15 20:04:42 +0900

    修正11/15②

--- src/main/java/com/dmm/task/MainController.java
+++ src/main/java/com/dmm/task/MainController.java
@@ -25,49 +25,54 @@ public class MainController {
 	@GetMapping("/main")
 	public String main() {
 
-	    // 週と日を格納する二次元配列を用意する
+	    // 週と日を格納する二次元のListを用意する
 	    List<List<LocalDate>> month = new ArrayList<>();
-	    
+
 	    // 1週間分のLocalDateを格納するListを用意する
 	    List<LocalDate> week = new ArrayList<>();
-	    
-	 // 日にちを格納する変数を用意する
+
+	    // 日にちを格納する変数を用意する
 	    LocalDate day;
 
 	    // その月の1日を取得する
 	    day = LocalDate.now();  // 現在日時を取得
 	    day = LocalDate.of(day.getYear(), day.getMonthValue(), 1);  // 現在日時からその月の1日を取得
-	    
+
 	    // 前月分の LocalDateを求める
 	    DayOfWeek w = day.getDayOfWeek();  // 当該日の曜日を取得
-	    day = day.minusDays(w.getValue());  // 1日からマイナスして 10/27を取得
-	    
-	    
-	    //繰り返す
-	    for(int i = 1; i <= 7; i++) {
-	    
-	    	//最終週かどうかの判定
-	    	if (条件式){
-	    		  
-	    		}else{
-	    		  
-	    		}
-	    	
-	    	// 1週目（1日ずつ増やして 週のリストに格納していく）
-	    	for(int j = 1; j <= 7; j++) {
-	    		week.add(day);  // 週のリストへ格納
-	    		day = day.plusDays(1);  // 1日進める
-	    	}
-	    	month.add(week);  // 1週目のリストを、月のリストへ格納する
+	    day = day.minusDays(w.getValue());  // 1日からマイナス
 
-	    	week = new ArrayList<>();  // 次週のリストを新しくつくる
-	    
-	    	
+	    // 1週目（1日ずつ増やして 週のリストに格納していく）
+	    for(int i = 1; i <= 7; i++) {
+	      week.add(day);  // 週のリストへ格納
+	      day = day.plusDays(1);  // 1日進める
+	    }    
+	    month.add(week);  // 1週目のリストを、月のリストへ格納する
+
+	    week = new ArrayList<>();  // 次週のリストを新しくつくる
+
+	    // 2週目（「7」から始めているのは2週目だから）
+	    for(int i = 7; i <= day.lengthOfMonth(); i++) {
+	      week.add(day);  // 週のリストへ格納
+
+	      w = day.getDayOfWeek();
+	      if(w == DayOfWeek.SATURDAY) {  // 土曜日だったら
+	        month.add(week);  // 当該週のリストを、月のリストへ格納する
+	        week = new ArrayList<>();  // 次週のリストを新しくつくる
+	      }
+
+	      day = day.plusDays(1);  // 1日進める
 	    }
+
+	    //続き
+	    //翌週
+	    day.lengthOfMonth();
+	       
+	    
+	    
 	    return "main";
 	   
 	}
-		
 	
 	@GetMapping("/login")
 	public String login() {
