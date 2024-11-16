23ca95415ce4b2136678ec04629ec2e4ee954caf
akihikoisoe <180363344+akiso999@users.noreply.github.com>
2024-11-16 23:54:43 +0900

    11/16修正no.4

--- src/main/java/com/dmm/task/repository/TasksRepository.java
+++ src/main/java/com/dmm/task/repository/TasksRepository.java
@@ -1,11 +1,16 @@
 package com.dmm.task.repository;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.dmm.task.entity.Tasks;
 
 public interface TasksRepository extends JpaRepository<Tasks, Integer>, JpaSpecificationExecutor<Tasks> {
 
-	  // 中身はタスク表示時に実装するため、ひとまず省略
+	  @Query("select a from Tasks a where a.date between :from and :to and name = :name")
+	  List<Tasks> findByDateBetween(@Param("from") LocalDateTime from, @Param("to") LocalDateTime to, @Param("name") String name);
 }
